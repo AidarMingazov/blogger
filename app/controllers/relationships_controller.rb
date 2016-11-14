51d34656d602927c5b1a14a1bedd576ee8c2e3a5
Айдар Мингазов <aydar@MacBook-Pro-Ajdar.local>
2016-11-14 21:50:42 +0300

    follow btn ajax request need fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,15 +3,24 @@ class RelationshipsController < ApplicationController
 
   def create
     user = User.find(params[:followed_id])
+    
     if user != current_user
     	current_user.follow(user)
-    end
-    redirect_to user
+	    respond_to do |format|
+        # bug
+        format.html { redirect_to @user }
+        format.js
+      end
+	  end
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    respond_to do |format|
+      # bug
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
