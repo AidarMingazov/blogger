24f488ca130b96a534d38651df83108ae3dfc3f8
Айдар Мингазов <aamingazov@gmail.com>
2016-11-15 15:50:33 +0300

    decorator logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
 	def index
-		@users = User.all
+		@users = User.all.decorate
 	end
 
 	def show
-		@user = User.find(params[:id])
+		@user = User.find(params[:id]).decorate
 	end
 
 end
