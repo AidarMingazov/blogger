4e8accb670993947250bd1f391d1e93097d99fc5
Айдар Мингазов <aamingazov@gmail.com>
2016-11-21 13:07:11 +0300

    User nickname null false

--- db/migrate/20161114094312_add_nickname_to_user.rb
+++ db/migrate/20161114094312_add_nickname_to_user.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :nickname, :string, unique: true
+    add_column :users, :nickname, :string, null: false, unique: true
   end
 end
