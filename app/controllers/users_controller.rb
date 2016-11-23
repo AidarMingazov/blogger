9d66c10ecbe17541c2ead0453b09034ac25232e1
Айдар Мингазов <aamingazov@gmail.com>
2016-11-23 13:21:12 +0300

    User watch in 'who_can_follow' only unfollow Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
 	def index
-		@users = User.where('id != ?', current_user.id).decorate
+    @users = User.where.not(id: current_user.following.map(&:id) << current_user.id).decorate
 	end
 
 	def show
-    @who_can_follow_users = User.where('id != ?', current_user.id).decorate
 		@user = User.find(params[:id]).decorate
     @feed = @user.feed
+    
     @user_post = @user.posts
+
+    @who_can_follow_users = User.where.not(id: current_user.following.map(&:id) << current_user.id).decorate
 	end
 
   def following
