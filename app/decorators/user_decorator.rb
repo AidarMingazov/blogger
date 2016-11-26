8aacb440ce59ea76e8dae1630fe15919b18a1a60
Айдар Мингазов <aamingazov@gmail.com>
2016-11-26 19:15:17 +0300

    add post owner to feed

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -9,6 +9,23 @@ class UserDecorator < ApplicationDecorator
     end
   end
 
+  def post_owner(post)
+    "#{ full_name_by_post(post) } #{ nickname_by_post(post) }".strip
+  end
+
+  def full_name_by_post(post)
+    user = User.find(post.user_id)
+    if user.first_name.blank? && user.last_name.blank?
+      'No name provided.'
+    else
+      "#{ user.first_name } #{ user.last_name }".strip
+    end
+  end
+
+  def nickname_by_post(post)
+    get_nickname(User.find(post.user_id))
+  end
+
   def at_nickname
     "@#{ nickname }"
   end
