60ecceea3116b578380623943ed75df389a2e494
Айдар Мингазов <aamingazov@gmail.com>
2016-11-27 13:05:17 +0300

    fix links|less user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,6 @@ class UsersController < ApplicationController
   expose_decorated :user
   expose_decorated :post
   
-	def index
-	end
-
-	def show
-	end
-
-  def edit
-  end
-
   def update
     if user.update(user_params)
       redirect_to user
@@ -21,17 +12,8 @@ class UsersController < ApplicationController
     end
   end
 
-  def followers
-  end
-
-  def following
-  end
-
-  def who_to_follow
-  end
-
   private
-
+  
     def set_who_to_follow
       @who_to_follow = User.where.not(id: current_user.following.map(&:id) << current_user.id)
     end
